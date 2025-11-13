b0669f434e7e31fa8b365b6dfed2ded141ec791b
sclimk <sclimk@gmail.com>
2025-11-13 15:25:06 -0800

    Add clock and adjust FPS

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 
 
