3a5220f4ff32796c4671760249b6afcdaccb3ff3
sclimk <sclimk@gmail.com>
2025-11-13 16:26:10 -0800

    Move around the screen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 from logger import log_state
 from player import Player
 
@@ -19,14 +19,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
     
-    
-
-
 
 if __name__ == "__main__":
     main()
