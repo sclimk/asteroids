c5c1ad2e89f3b7486c8b7f6d98035f7967fc4825
sclimk <sclimk@gmail.com>
2025-11-13 16:02:33 -0800

    drawing triangle player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 pygame.init()
 
@@ -12,15 +13,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
+    
+
 
 
 if __name__ == "__main__":
